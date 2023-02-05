dfdf29ec8d9a60eb511442bb10de3e062fc1d4d3
Tian <39994600+shinuki262012@users.noreply.github.com>
2023-02-05 00:13:29 +0000

    Added test 3D objects
    Attempt to display 3D objects(pyramids) over image.
    Experimented scaling and position offset.

--- app/src/main/java/com/example/pbrg_android/utility/rendering/AugmentedImageRenderer.java
+++ app/src/main/java/com/example/pbrg_android/utility/rendering/AugmentedImageRenderer.java
@@ -16,6 +16,7 @@
 package com.example.pbrg_android.utility.rendering;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.google.ar.core.Anchor;
 import com.google.ar.core.AugmentedImage;
@@ -40,29 +41,36 @@ public class AugmentedImageRenderer {
   private final ObjectRenderer imageFrameLowerLeft = new ObjectRenderer();
   private final ObjectRenderer imageFrameLowerRight = new ObjectRenderer();
 
+  private final ObjectRenderer pyramidRenderer = new ObjectRenderer();
+
   public AugmentedImageRenderer() {}
 
   public void createOnGlThread(Context context) throws IOException {
 
     imageFrameUpperLeft.createOnGlThread(
-        context, "models/frame_upper_left.obj", "models/frame_base.png");
+        context, "models/frame_upper_left.obj", "models/frame_base1.png");
     imageFrameUpperLeft.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f);
     imageFrameUpperLeft.setBlendMode(BlendMode.AlphaBlending);
 
     imageFrameUpperRight.createOnGlThread(
-        context, "models/frame_upper_right.obj", "models/frame_base.png");
+        context, "models/frame_upper_right.obj", "models/frame_base1.png");
     imageFrameUpperRight.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f);
     imageFrameUpperRight.setBlendMode(BlendMode.AlphaBlending);
 
     imageFrameLowerLeft.createOnGlThread(
-        context, "models/frame_lower_left.obj", "models/frame_base.png");
+        context, "models/frame_lower_left.obj", "models/frame_base1.png");
     imageFrameLowerLeft.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f);
     imageFrameLowerLeft.setBlendMode(BlendMode.AlphaBlending);
 
     imageFrameLowerRight.createOnGlThread(
-        context, "models/frame_lower_right.obj", "models/frame_base.png");
+        context, "models/frame_lower_right.obj", "models/frame_base1.png");
     imageFrameLowerRight.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f);
     imageFrameLowerRight.setBlendMode(BlendMode.AlphaBlending);
+
+    pyramidRenderer.createOnGlThread(
+            context, "models/pyramid80.obj" , "models/frame_base1.png");
+    pyramidRenderer.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f);
+    pyramidRenderer.setBlendMode(BlendMode.AlphaBlending);
   }
 
   public void draw(
@@ -73,6 +81,14 @@ public class AugmentedImageRenderer {
       float[] colorCorrectionRgba) {
     float[] tintColor =
         convertHexToColor(TINT_COLORS_HEX[augmentedImage.getIndex() % TINT_COLORS_HEX.length]);
+
+    final float mazeEdgeSize = 800.0f; // Magic number of pyramid size
+    final float maxImageEdgeSize = Math.max(augmentedImage.getExtentX(), augmentedImage.getExtentZ()); // Get largest detected image edge size
+
+
+    float mazeScaleFactor = maxImageEdgeSize / mazeEdgeSize; // scale to set Maze to image size
+    Log.e("Maze scale factor", "" + mazeScaleFactor);
+    float[] modelMatrix = new float[16];
 
     Pose[] localBoundaryPoses = {
       Pose.makeTranslation(
@@ -100,7 +116,6 @@ public class AugmentedImageRenderer {
     }
 
     float scaleFactor = 1.0f;
-    float[] modelMatrix = new float[16];
 
     worldBoundaryPoses[0].toMatrix(modelMatrix, 0);
     imageFrameUpperLeft.updateModelMatrix(modelMatrix, scaleFactor);
@@ -117,7 +132,47 @@ public class AugmentedImageRenderer {
     worldBoundaryPoses[3].toMatrix(modelMatrix, 0);
     imageFrameLowerLeft.updateModelMatrix(modelMatrix, scaleFactor);
     imageFrameLowerLeft.draw(viewMatrix, projectionMatrix, colorCorrectionRgba, tintColor);
+
+    Pose mazeModelLocalOffset = Pose.makeTranslation(
+            0.0f * mazeScaleFactor,
+            0.0f,
+            0.0f * mazeScaleFactor);
+    anchorPose.compose(mazeModelLocalOffset).toMatrix(modelMatrix, 0);
+    pyramidRenderer.updateModelMatrix(modelMatrix, mazeScaleFactor); // This line relies on a change in ObjectRenderer.updateModelMatrix later in this codelab.
+    pyramidRenderer.draw(viewMatrix, projectionMatrix, colorCorrectionRgba, tintColor);
   }
+// Adjust size of detected image and render it on-screen
+
+//  public void draw(
+//        float[] viewMatrix,
+//        float[] projectionMatrix,
+//        AugmentedImage augmentedImage,
+//        Anchor centerAnchor,
+//        float[] colorCorrectionRgba) {
+//  float[] tintColor =
+//          convertHexToColor(TINT_COLORS_HEX[augmentedImage.getIndex() % TINT_COLORS_HEX.length]);
+//    final float mazeEdgeSize = 492.65f; // Magic number of maze size
+//    final float maxImageEdgeSize = Math.max(augmentedImage.getExtentX(), augmentedImage.getExtentZ()); // Get largest detected image edge size
+//
+//    Pose anchorPose = centerAnchor.getPose();
+//
+//    float mazeScaleFactor = maxImageEdgeSize / mazeEdgeSize; // scale to set Maze to image size
+//    float[] modelMatrix = new float[16];
+//
+//    // OpenGL Matrix operation is in the order: Scale, rotation and Translation
+//    // So the manual adjustment is after scale
+//    // The 251.3f and 129.0f is magic number from the maze obj file
+//    // You mustWe need to do this adjustment because the maze obj file
+//    // is not centered around origin. Normally when you
+//    // work with your own model, you don't have this problem.
+//    Pose mazeModelLocalOffset = Pose.makeTranslation(
+//            -251.3f * mazeScaleFactor,
+//            0.0f,
+//            129.0f * mazeScaleFactor);
+//    anchorPose.compose(mazeModelLocalOffset).toMatrix(modelMatrix, 0);
+//  pyramidRenderer.updateModelMatrix(modelMatrix, mazeScaleFactor); // This line relies on a change in ObjectRenderer.updateModelMatrix later in this codelab.
+//  pyramidRenderer.draw(viewMatrix, projectionMatrix, colorCorrectionRgba, tintColor);
+//}
 
   private static float[] convertHexToColor(int colorHex) {
     // colorHex is in 0xRRGGBB format
